f8390c85adbb3d2a317ff9f31f60dd18ee4f4263
Swami Romão <swamiromao12@gmail.com>
2025-09-19 11:52:44 -0300

    Initial commit

---
+++ src/academy/maratonajava/introducao/Ola.java
@@ -0,0 +1,7 @@
+package academy.maratonajava.introducao;
+
+public class Ola {
+    public static void main(String[] args) {
+        System.out.println("Oi");
+    }
+}
---
+++ src/academy/maratonajava/introducao/OlaDEv.java
@@ -0,0 +1,7 @@
+package academy.maratonajava.introducao;
+
+public class OlaDEv {
+    public static void main(String[] args) {
+        System.out.println("Ola Dev");
+    }
+}
