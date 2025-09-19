422534a8e76d94ded9966e63e92c8c125eba947a
Swami Romão <swamiromao12@gmail.com>
2025-09-19 11:56:03 -0300

    add_test

--- src/academy/maratonajava/introducao/Ola.java
+++ src/academy/maratonajava/introducao/Ola.java
@@ -2,6 +2,6 @@ package academy.maratonajava.introducao;
 
 public class Ola {
     public static void main(String[] args) {
-        System.out.println("Oi");
+        System.out.println("Hello");
     }
 }
